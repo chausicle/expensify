bf58d2804d279535891cc786cc7aa89d40169325
Justin Chau <chausicle@gmail.com>
2020-09-17 17:39:28 -0700

    Install react-router-dom, configure webpack to serve index.html for any 404 response, and initialize some routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "normalize.css/normalize.css"
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Expensify-app boiler</p>, document.querySelector("#app"));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is form my dashboard component
+  </div>
+);
+
+const AddDashboardPage = () => (
+  <div>
+    This is form my add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is form my edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is form my help component
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={ExpenseDashboardPage} />
+      <Route path="/create" component={AddDashboardPage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.querySelector("#app"));
