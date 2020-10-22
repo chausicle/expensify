5716ad87422c18ef0bbf8af9a66371287f11881d
Justin Chau <chausicle@gmail.com>
2020-10-21 19:07:12 -0700

    Refractor code into individual files in their respective folders

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+const store = configureStore();
+
+// addExpense -> Water bill
+// addExpense -> Gas bill
+// setTextFilter -> bill (2 items) -> water (1 item)
+// getVisibleExpenses -> print visible ones to screen
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.querySelector("#app"));
