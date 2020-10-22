f508d59fe974ab58db864c262286e952b6f3fad3
Justin Chau <chausicle@gmail.com>
2020-10-21 19:37:35 -0700

    Create store instance, add expenses, set filterto text value water and display filtered expenses

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
 
-// addExpense -> Water bill
-// addExpense -> Gas bill
-// setTextFilter -> bill (2 items) -> water (1 item)
 // getVisibleExpenses -> print visible ones to screen
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
 
-console.log(store.getState());
+// addExpense -> Water bill
+store.dispatch(addExpense({ description: 'Water bill', amount: 200 }));
+// addExpense -> Gas bill
+store.dispatch(addExpense({ description: 'Gas bill', amount: 150 }));
+// setTextFilter -> bill (2 items) -> water (1 item)
+// store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.querySelector("#app"));
